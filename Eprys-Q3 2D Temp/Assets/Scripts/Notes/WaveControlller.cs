08a935db7bb39622a28521f27b73ff35020e94a0
MUKILTEO\1722758 <1722758@mukilteo.wednet.edu>
2025-03-03 10:11:12 -0800

    Fixed files and organized scripts. Lesson got in the way and couldn't start the tilemap with the imported templates, Z.

---
+++ Eprys-Q3 2D Temp/Assets/Scripts/Notes/WaveControlller.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class WaveControlller : MonoBehaviour
+{
+
+    public LessonWave lessonWave;
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+        for (int i = 0; i < lessonWave.enemyCount; ++i)
+        {
+
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
